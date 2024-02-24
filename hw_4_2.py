bbf15ce53300d23fdde1e5d92620e8a05ba08f77
Ruslan0473 <rasemenyuk@gmail.com>
2024-02-24 13:06:00 +0200

    task 2 done

---
+++ hw_4_2.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+def get_cats_info(path):
+    try:
+        with open(path) as file:
+            file_text = file.read()
+            cats_info = []
+            for line in file_text.split("\n"):
+                line = line.split(",")
+                cats_info.append({"id":line[0], "name":line[1], "age":line[2]})
+            return(cats_info)
+    except:
+        print(" The file does not exist")
+
+
+
+
+
+cats_info = get_cats_info("c:/Users/admin/Desktop/PYTHON/M_4/data_cats.txt")
+print(cats_info)
